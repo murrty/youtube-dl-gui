c12e45af8a5b08c04574f72fff0ecbdc5820478e
matty <10895809+murrty@users.noreply.github.com>
2020-02-21 03:17:31 -0600

    make the first time alert more coherent
    reading saves lives

--- Program.cs
+++ Program.cs
@@ -15,10 +15,18 @@ namespace youtube_dl_gui {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            #if DEBUG
+                IsDebug = true;
+            #else 
+                IsDebug = false;
+            #endif
+
             // boot determines if the application can proceed.
-            bool boot = false;
-            if (Properties.Settings.Default.firstTime) {
-                if (MessageBox.Show("youtube-dl-gui is a visual extension to youtube-dl and is not affiliated with them in any way.\n\nThis program (and I) does not condone piracy or illegally downloading of any video you do not own the rights to or is not in public domain.\n\nAny help regarding any problems when downloading anything illegal (in my jurisdiction) will be ignored. Click no if you agree.\n\nHave you read the above?", "youtube-dl-gui", MessageBoxButtons.YesNo) == DialogResult.No) {
+            bool AllowLaunch = false;
+
+            if (Properties.Settings.Default.firstTime && !IsDebug) {
+                if (MessageBox.Show("youtube-dl-gui is a visual extension to youtube-dl and is not affiliated with the developers of youtube-dl in any way.\n\nThis program (and I) does not condone piracy or illegally downloading of any video you do not own the rights to or is not in public domain.\n\nAny help regarding any problems when downloading anything illegal (in my jurisdiction) will be ignored. If you've read the above, click no to continue into the application. This message will not appear again.\n\nHave you read the above?", "youtube-dl-gui", MessageBoxButtons.YesNo) == DialogResult.No) {
                     Properties.Settings.Default.firstTime = false;
 
                     if (MessageBox.Show("Downloads are saved to your downloads folder by default, would you like to specify a different location now?\n(You can change this in the settings at any time)", "youtube-dl-gui", MessageBoxButtons.YesNo) == DialogResult.Yes) {
@@ -36,20 +44,15 @@ namespace youtube_dl_gui {
                     }
 
                     Properties.Settings.Default.Save();
-                    boot = true;
+                    AllowLaunch = true;
                 }
             }
             else {
-                boot = true;
+                AllowLaunch = true;
             }
 
-            if (boot) {
+            if (AllowLaunch) {
                 if (mtx.WaitOne(TimeSpan.Zero, true)) {
-                #if DEBUG
-                    IsDebug = true;
-                #else 
-                    IsDebug = false;
-                #endif
                     Application.Run(new frmMain());
                     mtx.ReleaseMutex();
                 }
