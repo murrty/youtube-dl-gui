422f00b0255d3514264e74abbd1af40a6aaf7a6b
matty <10895809+murrty@users.noreply.github.com>
2023-05-11 06:54:39 -0500

    add reddit video downloader user script

--- Addons/reddit video download button.user.js
+++ Addons/reddit video download button.user.js
@@ -1,1 +1,50 @@
-This is currently a placeholder and may or may not be moved/deleted.
+// ==UserScript==
+// @name        reddit video download button
+// @author      murrty
+// @match       http*://*.reddit.com/r/*
+// @grant       none
+// @version     1.0
+// @homepage    https://github.com/murrty/youtube-dl-gui
+// @updateURL   https://github.com/murrty/youtube-dl-gui/raw/master/Addons/reddit%20video%20download%20button.user.js
+// @downloadURL https://github.com/murrty/youtube-dl-gui/raw/master/Addons/reddit%20video%20download%20button.user.js
+// @description Adds a "download video" button to the post.
+// ==/UserScript==
+
+// This only works on old.reddit.com.
+// or if the old layout is enabled in the user settings.
+
+var VideoPlayers = document.getElementsByClassName("reddit-video-player-root");
+if (VideoPlayers.length > 0) {
+    var Protocol = "ytdlgui:";
+    var Url = " \"" + document.URL + "\"";
+    var TagLines = document.getElementsByClassName("flat-list buttons");
+    if (TagLines.length > 0 && TagLines[0] != null) {
+        var VideoDownloadItem = document.createElement('li');
+        var VideoLink = document.createElement('a');
+        VideoLink.id = "download-video";
+        VideoLink.title = "Downloads the video using youtube-dl-gui.";
+        VideoLink.href = Protocol + "v" + Url;
+        VideoLink.appendChild(document.createTextNode("download video"));
+        VideoDownloadItem.append(VideoLink);
+
+        var AudioDownloadItem = document.createElement('li');
+        var AudioLink = document.createElement('a');
+        AudioLink.id = "download-video-audio";
+        AudioLink.title = "Downloads the videos audio using youtube-dl-gui.";
+        AudioLink.href = Protocol + "a" + Url;
+        AudioLink.appendChild(document.createTextNode("download audio"));
+        AudioDownloadItem.append(AudioLink);
+
+        var CustomDownloadItem = document.createElement('li');
+        var CustomLink = document.createElement('a');
+        CustomLink.id = "download-video-custom";
+        CustomLink.title = "Downloads the video custom arguments using youtube-dl-gui.";
+        CustomLink.href = Protocol + "c" + Url;
+        CustomLink.appendChild(document.createTextNode("download custom"));
+        CustomDownloadItem.append(CustomLink);
+
+        TagLines[0].append(VideoDownloadItem);
+        TagLines[0].append(AudioDownloadItem);
+        TagLines[0].append(CustomDownloadItem);
+    }
+}
